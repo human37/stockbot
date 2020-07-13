18c94a3f0e514d23bfea6791663941ced4969237
Ammon Taylor <ammonx9@gmail.com>
2020-07-13 16:19:39 -0600

    now allows for the rsi and macd functionality, verified to be correct

--- stocks.py
+++ stocks.py
@@ -1,4 +1,7 @@
 import yahoo_fin.stock_info as yf
+from pandas import DataFrame 
+from stockstats import StockDataFrame
+from warnings import catch_warnings, simplefilter
 import numpy as np
 import datetime
 
@@ -16,7 +19,7 @@ class Stock:
     def getEarnings(self):
         return yf.get_earnings(self.stock)
 
-    # requires boolean parameter, set yearly=False for quarterly data
+    # set yearly=False for quarterly data
     def getCashFlow(self, yearly):
         return yf.get_cash_flow(self.stock, yearly)
 
@@ -24,43 +27,32 @@ class Stock:
     def getFinancials(self, yearly, quarterly):
         return yf.get_financials(self.stock, yearly, quarterly)
 
-    # returns stats on the stock, including the moving average, return on equity, etc.
-    def getStats(self):
-        return yf.get_stats(self.stock)
-
+    # returns all daily price information about the stock
     def getData(self, start_date, end_date):
         return yf.get_data(self.stock, start_date=start_date, end_date=end_date)
 
-    def getRSI(self, n = 14):
-        # this method will return the current RSI of the stock
-        end_date = datetime.datetime.now().date()
-        start_date = end_date - datetime.timedelta(days=n)
-        # makes a list of the closing price of the stock each day for the last n days (default 2 weeks)
-        prices = self.getData(start_date, end_date)['close'].values.tolist()
-        deltas = np.diff(prices)
-        seed = deltas[: n + 1]
-        up = seed[seed >= 0].sum() / n
-        down = -seed[seed < 0].sum() / n
-        rs = up / down
-        rsi = np.zeros_like(prices)
-        rsi[:n] = 100. - 100. / (1. + rs)
-        for i in range(n, len(prices)):
-            delta = deltas[i - 1]
-            if delta > 0:
-                upval = delta
-                downval = 0.
+    # set rsi_12 to false for rsi 6 day calculation, defaults to 12 day calculation
+    def getRSI(self, n = 200, rsi_12 = True):
+        # returns the current rsi indicator of the stock (uses the past 200 business days for calculation)
+        with catch_warnings():
+            # suppresses default warning from pandas
+            simplefilter('ignore')
+            end_date = datetime.datetime.now().date()
+            start_date = end_date - datetime.timedelta(days=n)
+            stock_data = self.getData(start_date, end_date)
+            if rsi_12:
+                rsi = StockDataFrame(stock_data).get('rsi_12')[-1]
             else:
-                upval = 0.
-                downval = -delta
-            up = (up * (n - 1) + upval) / n
-            down = (down * (n - 1) + downval) / n
-            rs = up / down
-            rsi[i] = 100. - 100. / (1. + rs)
+                rsi = StockDataFrame(stock_data).get('rsi_6')[-1]
         return rsi
 
-    def getMACD(self):
-        # this method will return the current MACD of the stock
-        pass
+    def getMACD(self, n = 200):
+        # returns the current macd indicator of the stock (uses the past 200 business days for calculation)
+        end_date = datetime.datetime.now().date()
+        start_date = end_date - datetime.timedelta(days=n)
+        stock_data = self.getData(start_date, end_date)
+        macd = StockDataFrame(stock_data).get('macd')[-1]
+        return macd
 
 
 # example for using the above class
@@ -68,3 +60,4 @@ if __name__ == '__main__':
     microsoft = Stock('MSFT')
     print('price:', microsoft.getPrice())
     print('rsi:', microsoft.getRSI())
+    print('macd:', microsoft.getMACD())
