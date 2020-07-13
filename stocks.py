c0141ebf3b2b3c49f5b9c0e13dccecaabfda588a
Ammon Taylor <ammonx9@gmail.com>
2020-07-13 16:22:16 -0600

    restructured some imports to be more space effecient

--- stocks.py
+++ stocks.py
@@ -3,7 +3,7 @@ from pandas import DataFrame
 from stockstats import StockDataFrame
 from warnings import catch_warnings, simplefilter
 import numpy as np
-import datetime
+from datetime import datetime, timedelta
 
 
 # class requires the stock ticker as a parameter, for example 'MSFT' for microsoft
@@ -37,8 +37,8 @@ class Stock:
         with catch_warnings():
             # suppresses default warning from pandas
             simplefilter('ignore')
-            end_date = datetime.datetime.now().date()
-            start_date = end_date - datetime.timedelta(days=n)
+            end_date = datetime.now().date()
+            start_date = end_date - timedelta(days = n)
             stock_data = self.getData(start_date, end_date)
             if rsi_12:
                 rsi = StockDataFrame(stock_data).get('rsi_12')[-1]
@@ -48,8 +48,8 @@ class Stock:
 
     def getMACD(self, n = 200):
         # returns the current macd indicator of the stock (uses the past 200 business days for calculation)
-        end_date = datetime.datetime.now().date()
-        start_date = end_date - datetime.timedelta(days=n)
+        end_date = datetime.now().date()
+        start_date = end_date - timedelta(days = n)
         stock_data = self.getData(start_date, end_date)
         macd = StockDataFrame(stock_data).get('macd')[-1]
         return macd
