cce74f3ecd0470f65348155aed5a57e89caf82e4
Ammon Taylor <ammonx9@gmail.com>
2020-07-18 21:14:16 -0600

    now formats the emails in a nicer way

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stocks import Stock, StockManager, User
 from time import sleep
+from datetime import datetime
 
 
 if __name__ == '__main__':
@@ -16,7 +17,8 @@ if __name__ == '__main__':
         print('Running...')
         rsi_changes = ammon.getStockManager().checkRSI()
         macd_changes = ammon.getStockManager().checkMACD()
-        email_message = ''
+        email_message = 'Subject: StockBot Update ' + str(datetime.now()) + ': \n'
+        updates = False
 
         if len(rsi_changes) != 0:
             for change in rsi_changes:
@@ -26,6 +28,7 @@ if __name__ == '__main__':
                 price = stock.getName() + ' price is currently $' + str(round(stock.getPrice(), 2)) + '\n'
                 email_message += message
                 email_message += price
+            updates = True
                 
         if len(macd_changes) != 0:
             for change in macd_changes:
@@ -35,13 +38,14 @@ if __name__ == '__main__':
                 price = stock.getName() + ' price is currently $' + str(round(stock.getPrice(), 2)) + '\n'
                 email_message += message
                 email_message += price
+            updates = True
 
-        if len(email_message) != 0:
-            print('New message information:')
-            print(email_message)
+        if updates:
+            print('New message information!')
+            print('Sending email...')
             ammon.sendEmail(email_message)
             print('Email sent successfully.')
         else:
             print('No message to send at this moment.')
-        
-        sleep(120)
+        print('Going to sleep now.')
+        sleep(60)
