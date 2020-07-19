071eb805445a960fb37a88e99aa843d38615c622
Ammon Taylor <ammonx9@gmail.com>
2020-07-19 16:24:02 -0600

    manages tanner and ammons stocks now

--- main.py
+++ main.py
@@ -12,12 +12,23 @@ if __name__ == '__main__':
             Stock('AAPL'),
             Stock('ACB'),
             Stock('SNAP'),
-            Stock('BABA')
+            Stock('SPOT')
+        ]
+    )
+    tanners_stocks = StockManager(
+        [
+            Stock('TSLA'),
+            Stock('XSPA'),
+            Stock('AMD'),
+            Stock('AAPL'),
+            Stock('SPOT')
         ]
     )
     ammon = User(ammons_stocks, 'ammonx9@gmail.com')
+    tanner = User(tanners_stocks, 'tannernthorne@gmail.com')
 
     while True:
         # checks all stocks in StockManager, cycles every minute
         ammon.checkStocks()
-        sleep(60)
+        tanner.checkStocks()
+        sleep(600)
